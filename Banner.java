6e2652a926f2d8fc0cba4582b72f8877ec0a9e2e
rozie-07 <rs2431@srmist.edu.in>
2026-02-17 13:38:20 +0530

    Coded to Render OOPS as Banner using Print Statements

--- Banner.java
+++ Banner.java
@@ -1,6 +1,11 @@
 public class Banner {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("OOOO   OOOO  PPPP   PPPP   SSSSS");
+        System.out.println("O   O  O   O P   P  P   P  S");
+        System.out.println("O   O  O   O PPPP   PPPP    SSS");
+        System.out.println("O   O  O   O P      P          S");
+        System.out.println("OOOO   OOOO  P      P      SSSSS");
     }
 }
